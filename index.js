0d9ef6ec254f8a7411fe1986bfcb043aba496f1b
Jon Langlois <jonathan-langlois@live.ca>
2020-05-23 11:09:17 -0700

    deploy

--- index.js
+++ index.js
@@ -130,5 +130,5 @@ io.on('connection', socket => {
 });
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/build/index.html'));
+	res.sendFile(path.join(__dirname, 'build/index.html'));
 });
